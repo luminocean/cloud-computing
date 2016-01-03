766520410f690628a888b6ca0ff6545f214cb04b
luminocean <282896922@qq.com>
2016-01-03 22:21:17 +0800

    添加写入HBase支持

--- dataclean/src/main/java/paper/Driver.java
+++ dataclean/src/main/java/paper/Driver.java
@@ -1,9 +1,12 @@
 package paper;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -22,24 +25,38 @@ public class Driver extends Configured implements Tool {
 			return -1;
 		}
 		
-		// 设置隔行读取
-		getConf().set("textinputformat.record.delimiter", "\n\n");
+		// job配置
+		Configuration config = getConf();
+		String tableName = config.get("table");
+		boolean outputToHBase = tableName != null?true:false;
+		if(outputToHBase){
+			config.set(TableOutputFormat.OUTPUT_TABLE, tableName);
+		}
+		config.set("textinputformat.record.delimiter", "\n\n"); // 设置隔行读取
 		
-		Job job = Job.getInstance(getConf(), "Data Clean");
+		Job job = Job.getInstance(config, "Data Clean");
 		job.setJarByClass(getClass());
 		
-		// 配置多源输入路径
-		MultipleInputs.addInputPath(job, 
+		// Mapper配置
+		MultipleInputs.addInputPath(job, // 配置多源输入路径
 				new Path(args[0]), // 文件夹路径
 				TextInputFormat.class, // 使用默认的TextInputFormat输入格式
 				BibTexMapper.class); // Mapper类
+		job.setMapOutputKeyClass(NullWritable.class);
+		job.setMapOutputValueClass(Paper.class);
 		
-		job.setReducerClass(PaperReducer.class);
-		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Paper.class);
+		// Reducer配置，区分输出到文本文件还是hbase
+		if(outputToHBase){
+			job.setReducerClass(HBaseReducer.class);
+			job.setOutputFormatClass(TableOutputFormat.class);
+			job.setOutputKeyClass(ImmutableBytesWritable.class);
+			job.setOutputValueClass(Mutation.class);
+		}else{
+			FileOutputFormat.setOutputPath(job, new Path(args[1])); // 配置输出路径
+			job.setOutputKeyClass(NullWritable.class);
+			job.setOutputValueClass(Paper.class);
+		}
 		
-		// 配置输出路径
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
