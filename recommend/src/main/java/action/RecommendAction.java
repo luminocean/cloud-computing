1391cbe112324e8ae5bc102a574ebe12d9b8da31
张鑫 <Vio@zx-Vio.local>
2016-03-15 23:25:59 +0800

    修改jsp页面

--- recommend/src/main/java/action/RecommendAction.java
+++ recommend/src/main/java/action/RecommendAction.java
@@ -1,5 +1,13 @@
 package action;
 
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class RecommendAction extends ActionSupport {
@@ -8,11 +16,22 @@ public class RecommendAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private List<String> list;
 	
 	public String recommend() throws Exception{
-		System.out.println("hello");
+		ActionContext context=ActionContext.getContext(); 
+		HttpServletRequest request = (HttpServletRequest)context.get(ServletActionContext.HTTP_REQUEST);     
+        String bookName=request.getParameter("literature");    
+		System.out.println(bookName);
 		return "success";
-		
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
 	}
 
 }
